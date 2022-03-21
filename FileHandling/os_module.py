0cc886a7457c8de13b2e55f27999e8ba76cec8a6
vishnukulkarni627 <vishnukulkarni627@gmail.com>
2022-03-21 21:11:18 +0530

    file

---
+++ FileHandling/os_module.py
@@ -0,0 +1,17 @@
+# In order to access directory related methods we have to import os module
+import os
+# getcwd: get current working directory!!!
+print(os.getcwd())
+# mkdir: It is used to create a new directory
+# os.mkdir("Sample")
+# rmdir: It is used to remove the directory
+# os.rmdir("Sample")
+# chdir: In order to change the directory from one to another
+# path = r"C:\Users\VISHNU\PycharmProjects\First-Commit\con_programs"
+# os.chdir(path)
+# print(os.getcwd())
+# In order to list out all the files present in the directory
+# print(os.listdir(path))
+# path1 = r"C:\Users\VISHNU\PycharmProjects\First-Commit\FileHandling"
+# os.chdir(path1)
+# print(os.getcwd())
